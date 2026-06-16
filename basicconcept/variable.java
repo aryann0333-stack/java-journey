e5a6d77f73b6b2c0bcaaa15626ff469869a0a79a
aryann0333-stack <aryann.0333@gmail.com>
2026-06-16 08:02:01 +0530

    variables done

---
+++ basicconcept/variable.java
@@ -0,0 +1,11 @@
+// variable act as container to store value
+
+class variable {
+    public static void main(String ar[]) {
+
+        int num1 = 4;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
