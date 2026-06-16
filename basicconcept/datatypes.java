a07b5d9308cdbeb22e11067d91eec8051697fa80
aryann0333-stack <aryann.0333@gmail.com>
2026-06-16 07:58:51 +0530

    Data types done

---
+++ basicconcept/datatypes.java
@@ -0,0 +1,21 @@
+// data types used in java (primitive)
+
+public class datatypes {
+    public static void main(String[] args) {
+        int num1 = 3;
+        byte by = 127;
+        short sh = 558;
+        long l = 324334l;
+
+        float f = 45.334f;
+        double d = 5.5654;
+
+        System.out.println("int = " + num1);
+        System.out.println("byte = " + by);
+        System.out.println("short = " + sh);
+        System.out.println("long = " + l);
+        System.out.println("float = " + f);
+        System.out.println("double = " + d);
+
+    }
+}
