1c3621a8032ba66e7ce5a5ac049aa9bff268f662
aryann0333-stack <aryann.0333@gmail.com>
2026-09-10 22:31:49 +0530

    multi level inheritance

---
+++ oops concept/AdvCalc.java
@@ -0,0 +1,9 @@
+public class AdvCalc extends Calc {
+    public int multi(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int div(int num1, int num2) {
+        return num1 / num2;
+    }
+}
